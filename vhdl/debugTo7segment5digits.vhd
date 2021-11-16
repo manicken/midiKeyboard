42ad4a8ed039822503ec3f7495d73fe83e0ac6b9
manicken <65423412+manicken@users.noreply.github.com>
2021-11-16 18:31:44 +0100

    add layout + fix folders

---
+++ vhdl/debugTo7segment5digits.vhd
@@ -0,0 +1,26 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.all;
+USE IEEE.STD_LOGIC_UNSIGNED.all;
+
+ENTITY debugTo7segment5digits IS
+    PORT (
+        rawDataIn: IN STD_LOGIC_VECTOR(14 downto 0); 
+        digit1dataOut: OUT STD_LOGIC_VECTOR(3 downto 0); -- digit one rightmost
+        digit2dataOut: OUT STD_LOGIC_VECTOR(3 downto 0);
+        digit3dataOut: OUT STD_LOGIC_VECTOR(3 downto 0);
+        digit4dataOut: OUT STD_LOGIC_VECTOR(3 downto 0);
+        keyDownOut: OUT STD_LOGIC
+        );
+END debugTo7segment5digits;
+
+ARCHITECTURE debugTo7segment5digits_arch OF debugTo7segment5digits IS
+    
+BEGIN
+    digit1dataOut(3 downto 0) <= rawDataIn(3 downto 0);
+    digit2dataOut(2 downto 0) <= rawDataIn(6 downto 4);
+    digit2dataOut(3) <= '0';
+    digit3dataOut(3 downto 0) <= rawDataIn(10 downto 7);
+    digit4dataOut(2 downto 0) <= rawDataIn(13 downto 11);
+    digit4dataOut(3) <= '0';
+    keyDownOut <= rawDataIn(14);
+END debugTo7segment5digits_arch;
---
+++ vhdl/sevenSegmentHexDecCharacterGenerator.vhd
@@ -0,0 +1,59 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.all;
+USE IEEE.STD_LOGIC_UNSIGNED.all;
+
+ENTITY sevenSegmentHexDecCharacterGenerator IS
+    PORT (
+        dataIn: IN STD_LOGIC_VECTOR(3 downto 0);
+        invOut: IN STD_LOGIC;
+        segmentDataOut: OUT STD_LOGIC_VECTOR(6 downto 0)
+        );
+END sevenSegmentHexDecCharacterGenerator;
+
+ARCHITECTURE sevenSegmentHexDecCharacterGenerator_arch OF sevenSegmentHexDecCharacterGenerator IS
+    SIGNAL segmentData: STD_LOGIC_VECTOR(6 downto 0);
+BEGIN
+    PROCESS(dataIn, invOut, segmentData)
+    
+    BEGIN
+    case dataIn is
+        when "0000" =>
+            segmentData <= "0111111";
+        when "0001" =>
+            segmentData <= "0000110";
+        when "0010" =>
+            segmentData <= "1011011";
+        when "0011" =>
+            segmentData <= "1001111";
+        when "0100" =>
+            segmentData <= "1100110";
+        when "0101" =>
+            segmentData <= "1101101";
+        when "0110" =>
+            segmentData <= "1111101";
+        when "0111" =>
+            segmentData <= "0100111";
+        when "1000" =>
+            segmentData <= "1111111";
+        when "1001" =>
+            segmentData <= "1101111";
+        when "1010" =>
+            segmentData <= "1110111";
+        when "1011" =>
+            segmentData <= "1111100";
+        when "1100" =>
+            segmentData <= "1011000";
+        when "1101" =>
+            segmentData <= "1011110";
+        when "1110" =>
+            segmentData <= "1111001";
+        when "1111" =>
+            segmentData <= "1110001";
+        --when others =>
+        --    segmentData <= "0000000";
+        end case;
+        for i in 0 to 6 loop 
+           segmentDataOut(i) <= invOut xor segmentData(i);
+        end loop;
+    END PROCESS;
+END sevenSegmentHexDecCharacterGenerator_arch;
